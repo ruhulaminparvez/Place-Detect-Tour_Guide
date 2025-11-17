1603903ba8e539b79fb22793e6a7c0183df87cdb
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2025-11-17 18:45:13 +0600

    rename folder and move files

--- Detect Landmark v2/PyCharm/LM_Detection.py
+++ Detect Landmark v2/PyCharm/LM_Detection.py
@@ -6,38 +6,13 @@ import numpy as np
 import pandas as pd
 from geopy.geocoders import Nominatim
 
-
-# set favicon and title
-st.set_page_config(page_title='AI Tour Guide', page_icon='🌎', layout='centered', initial_sidebar_state='auto')
-
-# hide Made with Streamlit 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# set logo image gif 
-st.image('./logo/logo.png', width=200)
-
-# set title
-st.write(
-    """
-    # AI Tour Guide - Places to Detect
-    """
-)
-st.write("Upload your image and get the predicted landmark and its address.")
-
-# model url and label url
 model_url = 'https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_asia_V1/1'
-label_url = 'https://www.gstatic.com/aihub/tfhub/labelmaps/landmarks_classifier_asia_V1_label_map.csv'
+# model_url = 'on_device_vision_classifier_landmarks_classifier_asia_V1_1'
 
-
-df = pd.read_csv(label_url)
+# label_url = 'https://www.gstatic.com/aihub/tfhub/labelmaps/landmarks_classifier_asia_V1_label_map.csv'
+labels = 'landmarks_classifier_asia_V1_label_map.csv'
+df = pd.read_csv(labels)
 labels = dict(zip(df.id, df.name))
-
 
 def image_processing(image):
     img_shape = (321, 321)
@@ -51,14 +26,16 @@ def image_processing(image):
     result = classifier.predict(img)
     return labels[np.argmax(result)],img1
 
-
 def get_map(loc):
     geolocator = Nominatim(user_agent="Your_Name")
     location = geolocator.geocode(loc)
     return location.address,location.latitude, location.longitude
 
-
 def run():
+    st.title("Landmark Recognition")
+    img = PIL.Image.open('logo.png')
+    img = img.resize((256,256))
+    st.image(img)
     img_file = st.file_uploader("Choose your Image", type=['png', 'jpg'])
     if img_file is not None:
         save_image_path = './Uploaded_Images/' + img_file.name
@@ -79,15 +56,4 @@ def run():
             st.map(df)
         except Exception as e:
             st.warning("No address found!!")
-
-if __name__ == '__main__':
-    run()
-
-
-
-
-
-
-
-
-
+run()
---
+++ Tour Guide (Web App)/README.md
@@ -0,0 +1,25 @@
+## Travel Advisor
+Travel Advisor is a web application that allows users to search for hotels and restaurants in a given city. The application is built using the following technologies:
+* [React](https://reactjs.org/)
+* [React Router](https://reacttraining.com/react-router/)
+* [Material UI](https://material-ui.com/)
+* [Rapid API](https://rapidapi.com/)
+* [Google Maps API](https://developers.google.com/maps/documentation/javascript/overview)
+* [Google Places API](https://developers.google.com/places/web-service/overview)
+* [Axios](https://axios-http.com/)
+
+## Setup
+To run this project, install it locally using npm:
+
+```
+$ cd ../travel-advisor
+$ npm install
+$ npm start
+```
+
+## Features
+* Search for hotels and restaurants in a given city
+* View hotel and restaurant details
+* View hotel and restaurant photos
+* View hotel and restaurant location on Google Maps
+
